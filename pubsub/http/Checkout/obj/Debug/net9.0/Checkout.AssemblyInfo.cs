dde2960efc296715e736d99dd037c89ca87bdb0b
sunwei <name_sunwei@outlook.com>
2024-12-02 15:17:11 +0800

    update

--- pubsub/http/Checkout/obj/Debug/net9.0/Checkout.AssemblyInfo.cs
+++ pubsub/http/Checkout/obj/Debug/net9.0/Checkout.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d490f56c69da32c8fc50571a61f751b41f7640")]
 [assembly: System.Reflection.AssemblyProductAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ state_management/http/EventService.DeleteState/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dapr.Client;
+
+string DAPR_STORE_NAME = "statestore";
+
+var client = new DaprClientBuilder().Build();
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => cts.Cancel();
+
+await client.DeleteStateAsync(DAPR_STORE_NAME, "order_1", cancellationToken: cts.Token);
